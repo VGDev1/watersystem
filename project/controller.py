da999a9250b7dd512d76f15cefb779105e152304
Victor <victor@educar.se>
2020-04-28 20:23:39 +0200

    Changed html

--- project/controller.py
+++ project/controller.py
@@ -16,6 +16,7 @@ class Controller:
         GPIO.setup(self.RELAY2, GPIO.OUT) # GPIO Assign mode
         GPIO.setup(self.RELAY3, GPIO.OUT) # GPIO Assign mode
         GPIO.setup(self.RELAY4, GPIO.OUT) # GPIO Assign mode
+        self.deactivateWater()
     
     def activateWater(self):
         self.run = True
@@ -29,14 +30,26 @@ class Controller:
             if currentTime < sequences[0]:
                 print("turn on group A")
                 GPIO.output(self.RELAY1, GPIO.LOW) # on
+                GPIO.output(self.RELAY2, GPIO.HIGH) # on
+                GPIO.output(self.RELAY3, GPIO.HIGH) # on
+                GPIO.output(self.RELAY4, GPIO.HIGH) # on
             elif currentTime < sequences[1]:
                 print("turn on group B")
+                GPIO.output(self.RELAY1, GPIO.HIGH) # on
                 GPIO.output(self.RELAY2, GPIO.LOW) # on
+                GPIO.output(self.RELAY3, GPIO.HIGH) # on
+                GPIO.output(self.RELAY4, GPIO.HIGH) # on
             elif currentTime < sequences[2]:
                 print("turn on group C")
+                GPIO.output(self.RELAY1, GPIO.HIGH) # on
+                GPIO.output(self.RELAY2, GPIO.HIGH) # on
                 GPIO.output(self.RELAY3, GPIO.LOW) # on
+                GPIO.output(self.RELAY4, GPIO.HIGH) # on
             elif currentTime < sequences[3]:
                 print("turn on group D")
+                GPIO.output(self.RELAY1, GPIO.HIGH) # on
+                GPIO.output(self.RELAY2, GPIO.HIGH) # on
+                GPIO.output(self.RELAY3, GPIO.HIGH) # on
                 GPIO.output(self.RELAY4, GPIO.LOW) # on
             else:
                 self.deactivateWater()
